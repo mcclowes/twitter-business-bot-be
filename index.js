d565833253393936d9f2b3078f72f731239c567d
Max Clayton Clowes <max@mcclowes.com>
2022-12-31 14:29:11 +0800

    init

--- index.js
+++ index.js
@@ -3,8 +3,39 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express & vercel");
+const prefix = "Red-hot business idea: "
+
+const randomItem = (items) => items[Math.floor(Math.random()*items.length)];
+
+const generateTweet = async () => {
+  console.log("Fetching data...");
+  
+  const data = await axios
+    .get(process.env.GOOGLE_SHEET_ENDPOINT)
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  
+  let twists = [] 
+  let ideas = [] 
+  let markets = [] 
+  
+  console.log(data)
+  
+  data.sheet1.forEach(item=>{
+    if(item.twists) { twists.push(item.twists) }
+    if(item.ideas) { ideas.push(item.ideas) }
+    if(item.markets) { markets.push(item.markets) }
+  })
+    
+  return prefix + randomItem(twists) + ' ' + randomItem(ideas) + ' for ' + randomItem(markets) + '.';
+}
+
+app.get("/", await (req, res) => {
+  res.send(generateTweet()listen);
 });
 
 app.listen(5000, () => {
